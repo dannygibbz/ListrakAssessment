5896ea39224016bf554d8eb0fc04da0412cc6ee2
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-11-06 04:07:43 -0500

    Pushing files from local repository

---
+++ script.js
@@ -0,0 +1,98 @@
+const openModalButtons = document.querySelectorAll("[data-modal-target]")
+const closeModalButtons = document.querySelectorAll("[data-close-button]")
+let signupContainer = document.getElementById("signupContainer")
+const overlay = document.getElementById("overlay")
+const email = document.getElementById("email")
+let inputContainer = document.getElementById("inputContainer")
+let signup = document.getElementById("signupButton")
+let input = document.getElementById("input")
+let message = document.getElementById("message")
+let body = document.getElementsByClassName("body")
+let modal = document.getElementById("modal")
+let secondModal = document.getElementsByClassName("secondModal-header")
+
+openModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active")
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal")
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal === null) return
+  modal.classList.add("active")
+  overlay.classList.add("active")
+}
+
+function closeModal(modal) {
+  if (modal === null) return
+  modal.classList.remove("active")
+  overlay.classList.remove("active")
+}
+
+email.addEventListener("click", () => {
+  inputContainer.style.display = "block"
+  email.style.display = "none"
+})
+
+signupButton.addEventListener("click", () => {
+  if (input.value === "") {
+    message.innerText = "Email field cannot be blank!"
+    message.style.display = "block"
+  }
+  if (input.value !== "" && input.value.length < 10) {
+    message.innerText = `Emails must contain 10 characters minimum.`
+    message.style.display = "block"
+  }
+  if (input.value !== "" && input.value.length >= 10) {
+    modal.innerHTML = `
+    <div class="secondModal-header">
+      <button data-close-button class="close-button">&times;</button>
+    </div>
+
+    <div>
+      <div id="contentContainer">
+        <div id="friendsMessage">Just Like Old Friends</div>
+        <div id="dots">
+          <div class="dot"></div>
+          <div class="line">
+            Hey there, Listrak Dev Team! This "line" design provides a nice
+            opportunity to cloak a small, personal message. Firstly, thanks so
+            much for reviewing my assessment. Your time and effort are much
+            appreciated! Secondly, if you'd like to discuss the design choices
+            I made and/or schedule another interview or pair coding exercise,
+            please send me an email at dgibbonswebdev@gmail.com. Have a great
+            day!
+          </div>
+          <div class="dot"></div>
+          <div class="bigDot"></div>
+          <div class="dot"></div>
+        </div>
+        <div id="secondaryMessage">
+          We'll keep you updated on any new deals, news, or gossip that comes
+          our way!
+        </div>
+        <div id="shopButton">
+          Go Shop!
+        </div>
+      </div>
+    </div>
+  </div>
+  
+`
+  }
+})
